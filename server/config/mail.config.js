e52c0f272c3dae348a7e7aa1d8ca8bf002180a71
bikram8538 <bikram8538@gmail.com>
2026-07-31 10:09:17 +0530

    transporter.veryfy()

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -10,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify((error) => {
     if (error) {
-        console.error("❌ SMTP Error:", error.message);
+        console.error("❌ SMTP Error:", error);
     } else {
         console.log("✅ SMTP Server Ready");
     }
